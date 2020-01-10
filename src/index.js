21a812e84ee95cfd33036cef0d303052761fe746
badogan <basri.dogan.71@gmail.com>
2020-01-10 15:57:19 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -43,6 +43,7 @@ function post(baseURI,bodyObject){
 }
 
 function patch(baseURI,id,bodyObject){
+  // debugger
   return fetch(`${baseURI}/${id}`,{
     method: "PATCH",
     headers: {
@@ -75,66 +76,50 @@ function postToyThenRenderToy(event){
   })
 }
 
-function createNewToy(toyName,toyImage){
-  let formData = {
-    name: toyName,
-    image: toyImage,
-    likes: 0
-  };
-  let configObj = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(formData)
-  };
-  return fetch(baseURI, configObj).then(response => response.json()).catch(error => function(error){
-    console.log(error.message)
-  })
-}
-
 // index page. render all toys
 function fetchToys(baseURI){
+  console.log("inside fetchToys")
   return fetch(baseURI).then(response => response.json()).then(json => renderToys(json)).catch(error => function(error){
     console.log(error.message)
   })
 }
 
 function renderToys(json){
-  json.forEach(toy => addAToyToPage(toy));
+  console.log("inside renderToys")
+  json.forEach(addAToyToPage);
 }
 
 function addAToyToPage(toy){
-  // console.log("inside addAToyToPage")
+  console.log("inside addAToyToPage")
   let newDiv = document.createElement('div')
   newDiv.classList.add('card')
   newDiv.id = toy.id
-  newH2 = document.createElement('h2')
+  let newH2 = document.createElement('h2')
   newH2.innerText = toy.name
-  newImg = document.createElement('img')
+  let newImg = document.createElement('img')
   newImg.src = toy.image
   newImg.classList.add('toy-avatar')
-  newP = document.createElement('p')
-  newP.id = `p-${toy.id}` //TODO: make sure this works!
+  let newP = document.createElement('p')
   newP.innerText = `${toy.likes} Likes`
-  newLikeButton = document.createElement('button')
+  newP.id = toy.id
+  let newLikeButton = document.createElement('button')
   newLikeButton.classList.add('like-btn')
   newLikeButton.innerText = "Like <3"
   newLikeButton.addEventListener("click",()=>patchLikesThenUpdateToy(toy.id,newP))
-  newDeleteButton = document.createElement('button')
+  let newDeleteButton = document.createElement('button')
   newDeleteButton.classList.add('delete-btn')
   newDeleteButton.innerText = 'Delete'
   newDeleteButton.addEventListener("click",()=>deleteToyFromServerAndThenFromClient(toy.id,newDiv))
   newDiv.append(newH2,newImg,newP,newLikeButton,newDeleteButton)
   toyCollection.appendChild(newDiv)
 }
+
 function patchLikesThenUpdateToy(id,newP){
   let bodyObject = {
     likes: parseInt(newP.innerText) + 1
   }
-  patch(baseURI,id, bodyObject).then(toy=>{
-    newP.innerText = `${toy.likes} Likes-updated`
+  patch(baseURI, id, bodyObject).then(toy => {
+    newP.innerText = `${toy.likes} Likes patched`
   })
 }
 
