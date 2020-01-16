00fddd9dd9f9f2a10e65e37d1ada397e52ca382c
github username <github email address>
2020-01-16 16:27:03 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,117 @@
-let addToy = false
-
 document.addEventListener("DOMContentLoaded", ()=>{
+
+
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
+  const appendToysDiv = document.querySelector("#toy-collection")
+  const baseUrl = "http://localhost:3000/toys/"
+
+
+  function get(url) {
+    return fetch(url)
+    .then(resp => resp.json())
+  }
+
+  function post(url, obj) {
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(obj)
+    })
+    .then(resp => resp.json())
+  }
+
+  function patch(url, id, obj) {
+    return fetch(`${url}${id}`, {
+      method: "PATCH",
+      headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+      },
+      body: JSON.stringify(obj)
+    }).then(resp => resp.json())
+  }
+
+
+  function destroy(url, id) {
+    return fetch(`${url}${id}`, {
+      method: "DELETE"
+    })
+  }
+
+  function getToys() {
+    get(baseUrl)
+    .then(toys => toys.forEach(renderToys))
+  }
+
+  function renderToys(toy) {
+    let div = document.createElement("div")
+    div.className = "card"
+
+    let h2 = document.createElement("h2")
+    h2.innerText = toy.name
+
+    let img = document.createElement("img")
+    img.src = toy.image
+    img.className = "toy-avatar"
+
+    let p = document.createElement("p")
+    p.innerText = `${toy.likes} likes`
+
+    let likeBtn = document.createElement("button")
+    likeBtn.className = "like-btn"
+    likeBtn.innerText = "Like <3"
+
+    let deleteBtn = document.createElement("button")
+    deleteBtn.innerText = "Delete"
+
+
+    div.append(h2, img, p, likeBtn, deleteBtn)
+    appendToysDiv.appendChild(div)
+
+    likeBtn.addEventListener("click", () => patchLikeThenRender(toy.id, p))
+    deleteBtn.addEventListener("click", () => deleteToyFromServerThenClient(toy, div))
+
+  }
+
+  function deleteToyFromServerThenClient(toy, div) {
+    destroy(baseUrl, toy.id).then(() => div.remove())
+  }
+
+  function postToyThenRenderToy(event) {
+    event.preventDefault()
+
+    let bodyObj = {
+      name: event.target.name.value,
+      image: event.target.image.value
+    }
+    post(baseUrl, bodyObj)
+    .then(toy => {
+      renderToys(toy)
+      toyForm.reset()
+    })
+  }
+
+  function patchLikeThenRender(id, p) {
+     
+      let bodyObj = {
+        likes: parseInt(p.innerText) + 1
+      }
+
+      patch(baseUrl, id, bodyObj).then(toy => {
+        p.innerText = `${toy.likes} likes`
+      })
+    
+  }
+
+
+   
+
+  let addToy = false
+
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
     addToy = !addToy
@@ -12,119 +121,15 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
-  const inputForm = document.querySelector(".add-toy-form")
-  inputForm.addEventListener("submit", function(e) {
-    let name = document.querySelector('input[name="name"]').value 
-    let image = document.querySelector('input[name="image"]').value
-     
-    let formData = {
-      name: name,
-      image: image,
-      likes: 0
-    };
-    let configObj = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify(formData)
-    };
-  fetch("http://localhost:3000/toys", configObj)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(object){
-      location.reload() //this refreshes page, as new toy is now part of index, it will load with
-    })                          //functions below
-    .catch(function(error){
-      alert("unauthorized");
-      console.log(error.message);
-    });                
-  });
-  getAllToys()
 
-  function getAllToys(){
-    const targetUrl = "http://localhost:3000/toys"
 
-    fetch(targetUrl)
-     .then(resp => resp.json())
-     .then(json => renderToys(json));
-    
-  }
 
-function renderToys(json) {
- const targetDiv = document.querySelector("#toy-collection")
-  for (const element of json) {
-  const newDiv = document.createElement('div')
-  newDiv.className = "card"
-  newDiv.id = element.id
-  
-  const newH2 = document.createElement('h2')
-  newH2.innerText = element.name 
-  newDiv.appendChild(newH2)
 
-  const newImg = document.createElement('img')
-  newImg.src = element.image
-  newImg.className = "toy-avatar"
-  newDiv.appendChild(newImg)
 
-  const newP = document.createElement('p')
-  newP.innerText = `${element.likes} Likes `
-  newDiv.appendChild(newP)
 
-  const newButton = document.createElement('button')
-  newButton.className = "like-btn"
-  newButton.innerText = "Like <3"
-  newButton.addEventListener("click", function(e){
-    // find what the current likes is
 
-    let targetId = e.target.parentNode.id
-    let constructedUrl = "http://localhost:3000/toys".concat("/").concat(targetId)
-    fetch(constructedUrl)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(object){
-      let currentLikes = object.likes    
+  getToys()
 
-    //construct the PATCH request and increment the likes
-    const newLikes = currentLikes +1
-    updateLikesForMe(newLikes,constructedUrl)
-  }) 
-
-  })
-  newDiv.appendChild(newButton)
-  targetDiv.appendChild(newDiv)
- }
-}
-//WORKING!
-function updateLikesForMe(newLikes,constructedUrl){
-let updateData = {
-  likes: newLikes
-};
-let configObj2 = {
-  method: "PATCH",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(updateData)
-};
-
-fetch(constructedUrl, configObj2)
-.then(function(response){
-  return response.json();
-})
-.then(function(object){
-  location.reload() //this refreshes page, as new toy is now part of index, it will load with
-})                          //functions below
-.catch(function(error){
-  alert("unauthorized");
-  console.log(error.message);
-});  
-}
-//WORKING! 
-
+  toyForm.addEventListener("submit", postToyThenRenderToy)
 
 })
